9922fa271bfc464f5de2f98e546511dc5c4d022e
Gonzalo Quero <gonzalo@quiqup.com>
2017-07-15 16:58:27 +0100

    Initial commit, just a default Mix Project

---
+++ lib/defmock.ex
@@ -0,0 +1,18 @@
+defmodule Defmock do
+  @moduledoc """
+  Documentation for Defmock.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Defmock.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/defmock_test.exs
@@ -0,0 +1,8 @@
+defmodule DefmockTest do
+  use ExUnit.Case
+  doctest Defmock
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
